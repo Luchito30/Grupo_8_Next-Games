7ee2fda410c443b1e9ac76a7cd08c21f5d116d8c
Luchito30 <luchodick24@gmail.com>
2023-01-10 06:12:35 -0300

    se crea rama y se genera archivos login

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
 
 app.listen(port,() => console.log('servidor corriendo en http://localhost:3030'));
 
