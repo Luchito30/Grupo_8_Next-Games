c5656bb93d610da2aea25ab69a5b84061c615d73
agustina <agustinaderek09@gmail.com>
2023-01-19 19:23:14 -0300

    detalle de producto

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const port = 3030;
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+
+app.get('/detalle-producto', (req, res) => res.sendFile(path.join(__dirname, 'views', 'detalle-producto.html')))
 
 app.listen(port,() => console.log('servidor corriendo en http://localhost:3030'));
 
