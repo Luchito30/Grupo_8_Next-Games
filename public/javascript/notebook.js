aae4bd30e4b280acb6366670d42e62fae7e943dd
Luchito30 <luchodick24@gmail.com>
2023-01-23 08:39:52 -0300

    termina carruseles para medias query y termina de darle los estilos al home

--- public/javascript/notebook.js
+++ public/javascript/notebook.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper-notebook1', {
     // Optional parameters
-    slidesPerView: 5,
+    slidesPerView: 3,
     spaceBrtween: 0,
     direction: 'horizontal',
     loop: true,
@@ -23,3 +23,107 @@ const swiper = new Swiper('.swiper-notebook1', {
       prevEl: '.swiper-button-prev',
     },
   });
+  
+  const swiper1 = new Swiper('.swiper-notebook2', {
+    // Optional parameters
+    slidesPerView: 4,
+    spaceBrtween: 0,
+    direction: 'horizontal',
+    loop: true,
+    autoplay:{
+      delay:5000,
+      pauseOnMouseEnter:true,
+      disableOnInteraction:false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+
+  const swiper2 = new Swiper('.swiper-notebook3', {
+    // Optional parameters
+    slidesPerView: 6,
+    spaceBrtween: 0,
+    direction: 'horizontal',
+    loop: true,
+    autoplay:{
+      delay:5000,
+      pauseOnMouseEnter:true,
+      disableOnInteraction:false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+
+  const swiper3 = new Swiper('.swiper-banner', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay:{
+      delay:5000,
+      pauseOnMouseEnter:true,
+      disableOnInteraction:false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  const swiper4 = new Swiper('.swiper-banner2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay:{
+      delay:3500,
+      pauseOnMouseEnter:true,
+      disableOnInteraction:false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  
