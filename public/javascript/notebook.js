e0530141a21406e8d66f2d0cd0e2612c7f5b346a
Luchito30 <luchodick24@gmail.com>
2023-01-24 01:32:46 -0300

    arregla problema de merge con los carruseles, y detalle de producto

--- public/javascript/notebook.js
+++ public/javascript/notebook.js
@@ -1,129 +1,128 @@
 const swiper = new Swiper('.swiper-notebook1', {
-    // Optional parameters
-    slidesPerView: 3,
-    spaceBrtween: 0,
-    direction: 'horizontal',
-    loop: true,
-    autoplay:{
-      delay:5000,
-      pauseOnMouseEnter:true,
-      disableOnInteraction:false,
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-      dynamicBullets:true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  
-  const swiper1 = new Swiper('.swiper-notebook2', {
-    // Optional parameters
-    slidesPerView: 4,
-    spaceBrtween: 0,
-    direction: 'horizontal',
-    loop: true,
-    autoplay:{
-      delay:5000,
-      pauseOnMouseEnter:true,
-      disableOnInteraction:false,
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-      dynamicBullets:true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  
+  // Optional parameters
+  slidesPerView: 3,
+  spaceBrtween: 0,
+  direction: 'horizontal',
+  loop: true,
+  autoplay:{
+    delay:5000,
+    pauseOnMouseEnter:true,
+    disableOnInteraction:false,
+  },
 
-  const swiper2 = new Swiper('.swiper-notebook3', {
-    // Optional parameters
-    slidesPerView: 6,
-    spaceBrtween: 0,
-    direction: 'horizontal',
-    loop: true,
-    autoplay:{
-      delay:5000,
-      pauseOnMouseEnter:true,
-      disableOnInteraction:false,
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-      dynamicBullets:true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
-  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
 
-  const swiper3 = new Swiper('.swiper-banner', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay:{
-      delay:5000,
-      pauseOnMouseEnter:true,
-      disableOnInteraction:false,
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-      dynamicBullets:true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-  const swiper4 = new Swiper('.swiper-banner2', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    autoplay:{
-      delay:4000,
-      pauseOnMouseEnter:true,
-      disableOnInteraction:false,
-    },
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-      dynamicBullets:true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+const swiper1 = new Swiper('.swiper-notebook2', {
+  // Optional parameters
+  slidesPerView: 4,
+  spaceBrtween: 0,
+  direction: 'horizontal',
+  loop: true,
+  autoplay:{
+    delay:5000,
+    pauseOnMouseEnter:true,
+    disableOnInteraction:false,
+  },
 
-  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+const swiper2 = new Swiper('.swiper-notebook3', {
+  // Optional parameters
+  slidesPerView: 6,
+  spaceBrtween: 0,
+  direction: 'horizontal',
+  loop: true,
+  autoplay:{
+    delay:5000,
+    pauseOnMouseEnter:true,
+    disableOnInteraction:false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+const swiper3 = new Swiper('.swiper-banner', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay:{
+    delay:5000,
+    pauseOnMouseEnter:true,
+    disableOnInteraction:false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper4 = new Swiper('.swiper-banner2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  autoplay:{
+    delay:4000,
+    pauseOnMouseEnter:true,
+    disableOnInteraction:false,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
