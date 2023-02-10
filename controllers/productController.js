43f98d17f445620b4635657cc73a8b3f3a864b3a
AdrianaGuitian <adriana.guitian@gmail.com>
2023-02-10 01:03:58 -0300

    Se trabaja en EditCRUD

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,16 +1,51 @@
 module.exports = {
-  
+
     carrito: (req, res) => {
         return res.render('products/carrito');
     },
     detalleproducto: (req, res) => {
         return res.render('products/detalle-producto');
     },
+    /* Update - Form to edit */
     edicion: (req, res) => {
-        return res.render('products/edicion');
+        const { id } = req.params;
+        const product = products.find(product => product.id === +id);
+        return res.render('products/edicion', {
+            ...product
+        });
     },
+    /* Update - Method to update */
+    update: (req, res) => {
+        const { id } = req.params
+        const product = products.find(product => product.id === +id);
+
+        const { name, discount, price, description, category } = req.body;
+
+        const productModified = {
+            id: +id,
+            name: name.trim(),
+            description: description.trim(),
+            price: +price,
+            discount: +discount,
+            image: product.image,
+            category
+        }
+
+        const productsModified = products.map(product => {
+            if (product.id === +id) {
+                return productModified
+            }
+
+            return product;
+        })
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(productsModified, null, 3), "utf-8");
+
+        return res.redirect("/products/detalle-producto/" + id)
+    },
+
     crearItem: (req, res) => {
         return res.render('products/crear-item');
-}
+    }
 }
 
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const {carrito,detalleproducto,edicion, crearItem} = require('../controllers/productController');
+const {carrito, detalleproducto, edicion, crearItem} = require('../controllers/productController');
 
 const router = express.Router();
 
+// CONTROLLER REQUIRE 
+const productController = require('../controllers/productController');
 
+ 
 router.get('/carrito', carrito)
 router.get('/detalle-producto', detalleproducto)
-router.get('/edicion', edicion)
+
+/* EDIT PRODUCT */
+router.get('/edicion/:id', productController.edicion);
+router.put('/edicion/:id', productController.update);
+
 router.get('/crear-item', crearItem)
 
 module.exports = router;
