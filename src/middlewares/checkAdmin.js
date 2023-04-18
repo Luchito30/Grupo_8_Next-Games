3e0272293d4512ccd3fce9ad9d0c8e04cd9fcfb8
Agustinaacost4 <agustinaderek09@gmail.com>
2023-04-18 13:16:52 -0300

    se arreglan errores de dashboard

--- src/middlewares/checkAdmin.js
+++ src/middlewares/checkAdmin.js
@@ -1,7 +1,10 @@
-module.exports = (req,res,next) => {
-    if(req.session.userLogin && req.session.userLogin.rol === 1){
-        next()
+module.exports = (req, res, next) => {
+    if (req.session.userLogin && req.session.userLogin.rol === 1) {
+      // User is logged in and has admin permissions
+      next();
+    } else {
+      // User is not logged in or does not have admin permissions
+      return res.redirect('/login');
     }
-    next()
-    //return res.redirect('/')
-}   
+  }
+  
