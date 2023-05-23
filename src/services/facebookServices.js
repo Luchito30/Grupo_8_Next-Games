73dc7ec09afac9162325a33ce74dd16078ad6ec4
Luchito30 <luchodick24@gmail.com>
2023-05-23 07:54:38 -0300

    facebook

--- src/services/facebookServices.js
+++ src/services/facebookServices.js
@@ -1,8 +1,8 @@
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 
-const clientID = "774869420790758"
-const clientSecret = "bbf7cc4c05d99dbe147e3384d554432e"
+const clientID = "633357834994765"
+const clientSecret = "a4e279e07cb38ca386f649a3479ffb00"
 const callbackURL = "https://nextgames.onrender.com/auth/facebook/callback"
 
 const strategyConfig = new FacebookStrategy(
