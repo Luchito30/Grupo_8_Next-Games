acda549b23125a8d20ba127ca73ec5b23b0363e6
Luchito30 <luchodick24@gmail.com>
2023-06-06 04:30:52 -0300

    multiplicar quantity * cuotas, estilos

--- public/javascripts/addproductToCard.js
+++ public/javascripts/addproductToCard.js
@@ -5,7 +5,112 @@ const btnMoreProduct = document.querySelector('#btn-moreProduct');
 const cuotasSelect = document.querySelector('#cuotas');
 const precioElement = document.querySelector('.detalle__precio--descuento');
 const precioSinCambioElement = document.querySelector('.detalle__precio--sincambio');
+const botonCuotas = document.querySelector('#btn-updateCuotas')
 const URL_API_SERVER = "http://localhost:3000/api";
+
+
+function toThousand(value) {
+  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
+
+const tarjetasDetalle = document.querySelector('.tarjetas-detalle');
+const price = parseFloat(tarjetasDetalle.dataset.price);
+const discount = parseFloat(tarjetasDetalle.dataset.discount);
+
+function getCuotaPrice(cuotaValue, price, discount) {
+  if (discount) {
+    const priceWithDiscount = price - (price * discount) / 100;
+    switch (cuotaValue) {
+      case '1 pago':
+        return `$${toThousand(Math.round(priceWithDiscount))}`;
+      case '3 pago':
+        return `3 cuotas $${toThousand(Math.round(priceWithDiscount / 3))}`;
+      case '6 pago':
+        return `6 cuotas $${toThousand(Math.round(priceWithDiscount / 6))}`;
+      case '9 pago':
+        return `9 cuotas $${toThousand(Math.round(priceWithDiscount / 9))}`;
+      case '12 pago':
+        return `12 cuotas $${toThousand(Math.round(priceWithDiscount / 12))}`;
+      default:
+        return '';
+    }
+  } else {
+    switch (cuotaValue) {
+      case '1 pago':
+        return `$${toThousand(Math.round(price))}`;
+      case '3 pago':
+        return `3 cuotas $${toThousand(Math.round(price / 3))}`;
+      case '6 pago':
+        return `6 cuotas $${toThousand(Math.round(price / 6))}`;
+      case '9 pago':
+        return `9 cuotas $${toThousand(Math.round(price / 9))}`;
+      case '12 pago':
+        return `12 cuotas $${toThousand(Math.round(price / 12))}`;
+      default:
+        return '';
+    }
+  }
+}
+
+botonCuotas.addEventListener('click', () => {
+  const cuotaValue = cuotasSelect.value;
+  const cuotaPrice = getCuotaPrice(cuotaValue, price, discount);
+
+  if (precioElement) {
+    if (cuotaPrice !== '') {
+      precioElement.textContent = cuotaPrice;
+    } else {
+      precioElement.textContent = 'Sin descuento';
+    }
+  } else if (precioSinCambioElement) {
+    if (cuotaPrice !== '') {
+      precioSinCambioElement.textContent = cuotaPrice;
+    } else {
+      precioSinCambioElement.textContent = 'Sin descuento';
+    }
+  }
+});
+
+
+
+function calcularPrecioTotal(cantidad, cuotas, price, discount) {
+  if (discount) {
+    const priceWithDiscount = price - (price * discount) / 100;
+    const precioProducto = priceWithDiscount * cantidad;
+
+    switch (cuotas) {
+      case '1 pago':
+        return `$${toThousand(Math.round(precioProducto))}`;
+      case '3 pago':
+        return `3 cuotas de $${toThousand(Math.round(precioProducto / 3))}`;
+      case '6 pago':
+        return `6 cuotas de $${toThousand(Math.round(precioProducto / 6))}`;
+      case '9 pago':
+        return `9 cuotas de $${toThousand(Math.round(precioProducto / 9))}`;
+      case '12 pago':
+        return `12 cuotas de $${toThousand(Math.round(precioProducto / 12))}`;
+      default:
+        return '';
+    }
+  } else {
+    const precioProducto = price * cantidad;
+
+    switch (cuotas) {
+      case '1 pago':
+        return `$${toThousand(Math.round(precioProducto))}`;
+      case '3 pago':
+        return `3 cuotas de $${toThousand(Math.round(precioProducto / 3))}`;
+      case '6 pago':
+        return `6 cuotas de $${toThousand(Math.round(precioProducto / 6))}`;
+      case '9 pago':
+        return `9 cuotas de $${toThousand(Math.round(precioProducto / 9))}`;
+      case '12 pago':
+        return `12 cuotas de $${toThousand(Math.round(precioProducto / 12))}`;
+      default:
+        return '';
+    }
+  }
+}
 
 
 btnAddCart.addEventListener('click',async () => {
@@ -84,7 +189,24 @@ btnLessProduct.addEventListener('click', async (event) => {
       console.log(error);
     }
   }
-});
+
+  const cuotaValue = cuotasSelect.value;
+    const precioTotal = calcularPrecioTotal(quantity, cuotaValue, price, discount);
+
+    if (precioElement) {
+      if (precioTotal !== '') {
+        precioElement.textContent = precioTotal;
+      } else {
+        precioElement.textContent = 'Sin descuento';
+      }
+    } else if (precioSinCambioElement) {
+      if (precioTotal !== '') {
+        precioSinCambioElement.textContent = precioTotal;
+      } else {
+        precioSinCambioElement.textContent = 'Sin descuento';
+      }
+    }
+  })
 
 btnMoreProduct.addEventListener('click', async (event) => {
   const id = event.target.getAttribute('data-moreProduct');
@@ -116,66 +238,21 @@ btnMoreProduct.addEventListener('click', async (event) => {
   } catch (error) {
     console.log(error);
   }
-});
 
-const tarjetasDetalle = document.querySelector('.tarjetas-detalle');
-const price = parseFloat(tarjetasDetalle.dataset.price);
-const discount = parseFloat(tarjetasDetalle.dataset.discount);
-
-function getCuotaPrice(cuotaValue, price, discount) {
-  if (discount) {
-    const priceWithDiscount = price - (price * discount) / 100;
-    switch (cuotaValue) {
-      case '1 pago':
-        return `$${toThousand(Math.round(priceWithDiscount))}`;
-      case '3 pago':
-        return `3 cuotas de $${toThousand(Math.round(priceWithDiscount / 3))}`;
-      case '6 pago':
-        return `6 cuotas de $${toThousand(Math.round(priceWithDiscount / 6))}`;
-      case '9 pago':
-        return `9 cuotas de $${toThousand(Math.round(priceWithDiscount / 9))}`;
-      case '12 pago':
-        return `12 cuotas de $${toThousand(Math.round(priceWithDiscount / 12))}`;
-      default:
-        return '';
-    }
-  } else {
-    switch (cuotaValue) {
-      case '1 pago':
-        return `$${Math.round(price).toFixed(2)}`;
-      case '3 pago':
-        return `3 cuotas de $${toThousand(Math.round(price / 3))}`;
-      case '6 pago':
-        return `6 cuotas de $${toThousand(Math.round(price / 6))}`;
-      case '9 pago':
-        return `9 cuotas de $${toThousand(Math.round(price / 9))}`;
-      case '12 pago':
-        return `12 cuotas de $${toThousand(Math.round(price / 12))}`;
-      default:
-        return '';
-    }
-  }
-}
-
-cuotasSelect.addEventListener('change', () => {
   const cuotaValue = cuotasSelect.value;
-  const cuotaPrice = getCuotaPrice(cuotaValue, price, discount);
+  const precioTotal = calcularPrecioTotal(quantity, cuotaValue, price, discount);
 
   if (precioElement) {
-    if (cuotaPrice !== '') {
-      precioElement.textContent = cuotaPrice;
+    if (precioTotal !== '') {
+      precioElement.textContent = precioTotal;
     } else {
       precioElement.textContent = 'Sin descuento';
     }
   } else if (precioSinCambioElement) {
-    if (cuotaPrice !== '') {
-      precioSinCambioElement.textContent = cuotaPrice;
+    if (precioTotal !== '') {
+      precioSinCambioElement.textContent = precioTotal;
     } else {
       precioSinCambioElement.textContent = 'Sin descuento';
     }
   }
 });
-
-function toThousand(value) {
-  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-}
