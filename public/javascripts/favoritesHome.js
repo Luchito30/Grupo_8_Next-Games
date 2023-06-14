d33802715caea876673769c201c7e6b01fee5826
Luchito30 <luchodick24@gmail.com>
2023-06-13 21:11:02 -0300

    addproduct, favorites

--- public/javascripts/favoritesHome.js
+++ public/javascripts/favoritesHome.js
@@ -1,5 +1,5 @@
 const userId = document.body.getAttribute("data-userId");
-const URL_API_SERVER = "https://nextgames.onrender.com/api";
+const URL_API_SERVER = "http://localhost:3000/api";
 
 const toggleFavorite = async (id, { target }) => {
   try {
@@ -53,4 +53,31 @@ const toggleFavorite = async (id, { target }) => {
 } catch (error) {
   console.log(error);
 }
-};
+}
+
+const addProductToCart = async (id) => {
+    try {
+      const objProductId = {
+        productId: id,
+      };
+      const { ok } = await fetch(`${URL_API_SERVER}/cart/addProduct`, {
+        method: "POST",
+        body: JSON.stringify(objProductId),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+  
+      await Swal.fire({
+        title: ok ? "Producto agregado al carrito" : "Debes iniciar sesión",
+        icon: ok ? "success" : "warning",
+        showConfirmButton: false,
+        timer: 1200,
+      });
+  
+      !ok && (location.href = "/users/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  ;
